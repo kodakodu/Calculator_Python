4b4719ac60eebf558e2813f968cb5915391556e2
kodakodu <kodakodu@gmail.com>
2025-07-11 16:41:40 +0200

    1:13:50

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from PyQt5.QtWidgets import QApplication,QWidget, QLineEdit, QPushButton, QHBoxL
 # App settings
 app =QApplication([])
 main_window = QWidget()
+main_window.setWindowTitle("Calculator App")
+main_window.resize(250,300)
+
 
 # Design
 
 # Show/Run
+main_window.show()
+app.exec()
