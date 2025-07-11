d1b7bd34548f2368973d26e7ff99607ae6d796cf
kodakodu <kodakodu@gmail.com>
2025-07-11 16:38:03 +0200

    1:12:58

---
+++ main.py
@@ -0,0 +1,9 @@
+# Imports
+from PyQt5.QtWidgets import QApplication,QWidget, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QGridLayout
+# App settings
+app =QApplication([])
+main_window = QWidget()
+
+# Design
+
+# Show/Run
