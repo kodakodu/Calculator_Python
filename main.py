9a6a806c61028c056ab8e0869401fa98fa3acda9
kodakodu@gmail.com <kodakodu@gmail.com>
2025-07-15 14:12:54 +0200

    1:17:41

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ main_window.resize(250,300)
 text_box = QLineEdit()
 grid = QGridLayout()
 
-buttons = ["7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+"]
-
+buttons = [
+    "7","8","9","/",
+    "4","5","6","*",
+    "1","2","3","-",
+    "0",".","=","+"
+    ]
+clear = QPushButton("Clear")
+delete = QPushButton("<")
 
 # Design
+
+
 
 # Show/Run
 main_window.show()
